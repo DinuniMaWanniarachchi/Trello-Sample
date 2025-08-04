a03234a7a3ff26b508a8184a495adcbe834f87d2
Dinuni <dinuniwanniarachchi@gmail.com>
2025-08-04 11:20:11 +0530

    fix several errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { i18n } = require('./next-i18next.config');
-
 const nextConfig = {
-  i18n,
   reactStrictMode: true,
-  swcMinify: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import 'antd/dist/reset.css';
-
 import { I18nProvider } from '@/components/I18nProvider';
 
 const geistSans = Geist({
@@ -33,11 +32,6 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </I18nProvider>
-      </body>
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
       </body>
     </html>
   );
